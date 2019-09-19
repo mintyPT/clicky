9996fa0d0d00bedc9c6ceaeb195a4feb956f20a2
mintyPT <mintyPT@gmail.com>
2019-09-19 23:20:29 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import _ from "lodash";
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
 
 const ToggleContent = ({ toggle, content }) => {
@@ -16,80 +16,114 @@ const ToggleContent = ({ toggle, content }) => {
   );
 };
 
-const id = () =>
+const id = t =>
+  t +
+  "-" +
   String(Math.random())
     .split(".")
     .pop();
 
 const componentsTree = {
   text: {
-    generator: () => ({
-      id: "text-" + id(),
+    generator: (s = {}) => ({
+      id: id("text"),
       type: "text",
-      props: { text: "Hello!", color: "red-400", size: "4xl" }
+      props: { text: "Hello!", color: "red-400", size: "4xl", ...s }
     })
   },
   button: {
-    generator: () => ({
-      id: "button-" + id(),
+    generator: (s = {}) => ({
+      id: id("button"),
       type: "button",
-      props: { text: "my button", url: "http://sapo.pt" }
+      props: { text: "my button", url: "http://sapo.pt", ...s }
+    })
+  },
+  buttons: {
+    allow: ["button"],
+    generator: ({ ...s } = {}, children) => ({
+      id: id("buttons"),
+      type: "buttons",
+      props: {
+        styleChildren: {
+          border: "1px solid #333",
+          marginRight: "20px",
+          borderRadius: "500px",
+          padding: "5px 10px",
+          display: "inline-block",
+          fontWeight: "bold"
+        },
+        ...s
+      },
+      children
+    })
+  },
+  wrapper: {
+    allow: ["wrapper", "buttons", "text"],
+    generator: (s, children) => ({
+      id: id("wrapper"),
+      type: "wrapper",
+      children
     })
   }
 };
+const CustomizeElementWrapper = ({
+  As = "div",
+  children,
+  active = false,
+  type,
+  style = {},
+  ...etc
+}) => (
+  <As
+    {...etc}
+    className={"target " + (active ? "active" : "")}
+    style={{ margin: 5, ...style }}
+  >
+    {children}
+  </As>
+);
 
 class Disp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedElement: undefined,
+      selectedId: undefined,
+      selected: undefined,
       elements: [
-        {
-          id: "aaa",
-          type: "wrapper",
-          children: [
-            componentsTree.text.generator(),
-            {
-              id: "blablabla",
-              type: "buttons",
-              props: {
-                styleChildren: { border: "1px solid red", marginRight: "20px" }
-              },
-              children: [
-                componentsTree.button.generator(),
-                componentsTree.button.generator()
-              ]
-            }
-          ]
-        }
+        componentsTree.wrapper.generator({}, [
+          componentsTree.text.generator(),
+          componentsTree.buttons.generator({}, [
+            componentsTree.button.generator({ text: "facebook" }),
+            componentsTree.button.generator({ text: "twitter" })
+          ])
+        ])
       ]
     };
   }
 
-  addElement(id, type) {
-    this.setState({ selectedElement: { id, type } });
-    /*let elements = this.state.elements;
-
-    const loop = element => {
-      if (element.children) {
-        element.children = element.children.map(loop);
+  selectElement(id, type) {
+    let selected;
+    processTree(e => {
+      if (e.id === id) {
+        selected = e;
       }
-      if (element.id === id) {
-        const comp = componentsTree[type];
-        if (comp) {
-          element.children.push(comp.generator());
-        }
-      }
-      return element;
-    };
+    }, this.state.elements);
 
-    elements = elements.map(loop);
+    this.setState({ selectedId: id, selected });
+  }
 
-    this.setState({ elements });*/
+  action(id, action) {
+    this.setState({
+      elements: processTree(
+        e => (e.id === id ? elementAction(action, e) : e),
+        this.state.elements
+      )
+    });
   }
 
   render() {
     const elements = this.state.elements;
+    const activeId = this.state.selectedId;
 
     const RenderButton = ({ element, parent }) => {
       let style = {};
@@ -103,68 +137,55 @@ class Disp extends React.Component {
       );
     };
 
-    const CustomizeElementWrapper = ({ As = "div", id, type, children }) => (
-      <As
-        style={{ border: "1px solid green", padding: 3 }}
-        onClick={e => {
-          e.stopPropagation();
-          this.addElement(id, type);
-        }}
-      >
-        {children}
-      </As>
-    );
-
     const render = (element, parent) => {
+      const isActive = element.id === activeId;
+
+      const onClick = e => {
+        e.stopPropagation();
+        this.selectElement(element.id);
+      };
+
+      let inner;
+      const innerProps = {};
       switch (element.type) {
         case "button":
-          return (
-            <CustomizeElementWrapper
-              id={element.id}
-              type={element.type}
-              key={element.id}
-              As="span"
-            >
-              <RenderButton element={element} parent={parent} />
-            </CustomizeElementWrapper>
-          );
+          inner = <RenderButton element={element} parent={parent} />;
+          innerProps.style = { display: "inline-block" };
+          break;
 
         case "buttons":
           const children = element.children.map(el => render(el, element));
-
-          return (
-            <CustomizeElementWrapper
-              id={element.id}
-              type={element.type}
-              key={element.id}
-            >
-              {children}
-            </CustomizeElementWrapper>
-          );
+          inner = <div>{children}</div>;
+          break;
 
         case "wrapper":
-          return (
-            <CustomizeElementWrapper
-              id={element.id}
-              type={element.type}
-              key={element.id}
-            >
-              {element.children.map(el => render(el, element))}
-            </CustomizeElementWrapper>
-          );
+          inner = element.children.map(el => render(el, element));
+          break;
+
         case "text": {
           let classNames = "";
           classNames = classNames + " text-" + element.props.color;
           classNames = classNames + " text-" + element.props.size;
-          return (
-            <p key={element.id} className={classNames}>
-              {element.props.text}
-            </p>
-          );
+          inner = <span className={classNames}>{element.props.text}</span>;
+          innerProps.style = { display: "inline-block" };
+          break;
         }
         default:
           return "Missing render for " + element.type;
       }
+
+      return (
+        <CustomizeElementWrapper
+          active={isActive}
+          id={element.id}
+          type={element.type}
+          key={element.id}
+          onClick={onClick}
+          {...innerProps}
+        >
+          {inner}
+        </CustomizeElementWrapper>
+      );
     };
 
     return (
@@ -174,15 +195,64 @@ class Disp extends React.Component {
           content={({ show, hide }) => (
             <p>
               <Modal>
-                {JSON.stringify(this.state.selectedElement)}
-                <fieldset>
-                  <legend>Add elements</legend>
-                  ---
-                </fieldset>
-                <fieldset>
-                  <legend>Customize</legend>
-                  ---
-                </fieldset>
+                {JSON.stringify(this.state.selectedId)}
+                {this.state.selectedId && (
+                  <div>
+                    <fieldset>
+                      <legend>Add elements</legend>
+                      <button
+                        style={{ background: "#333333" }}
+                        onClick={() =>
+                          this.action(this.state.selectedId, {
+                            type: "add",
+                            component: componentsTree.text.generator()
+                          })
+                        }
+                      >
+                        add text
+                      </button>
+                      <button
+                        style={{ background: "#333333" }}
+                        onClick={() =>
+                          this.action(this.state.selectedId, {
+                            type: "add",
+                            component: componentsTree.button.generator()
+                          })
+                        }
+                      >
+                        add button
+                      </button>
+                      ---
+                    </fieldset>
+                    <fieldset>
+                      <legend>Customize</legend>
+                      ---
+                    </fieldset>
+
+                    <button
+                      style={{ background: "#333333" }}
+                      onClick={() =>
+                        this.action(this.state.selectedId, { type: "delete" })
+                      }
+                    >
+                      Delete element
+                    </button>
+
+                    <button
+                      style={{ background: "#333333" }}
+                      onClick={() =>
+                        this.action(this.state.selectedId, {
+                          type: "update_props",
+                          key: "styleChildren.border",
+                          value: "1px solid red"
+                        })
+                      }
+                    >
+                      Update border
+                    </button>
+                  </div>
+                )}
+
                 <button onClick={hide}>Close</button>
               </Modal>
             </p>
@@ -211,3 +281,52 @@ function App() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+/**
+ *
+ */
+const processTree = (cb, element) => {
+  element = _.cloneDeep(element);
+
+  if (_.isArray(element)) {
+    return element.map(e => processTree(cb, e)).filter(v => !!v);
+  }
+
+  if (element.children) {
+    element.children = processTree(cb, element.children);
+  }
+
+  return cb(element);
+};
+
+/**
+ *
+ * @param {int} id id of the element to change
+ * @param {function} elementAction function to call with the element we are looking for
+ * @param {*} element element we are currently checking
+ */
+const elementAction = (action, element) => {
+  switch (action.type) {
+    case "add":
+      return elementActionAdd(element, action);
+    case "delete":
+      return undefined;
+    case "update_props":
+      return elementActionUpdateProps(action, element);
+    default:
+      break;
+  }
+
+  return element;
+};
+
+function elementActionUpdateProps(action, element) {
+  const key = "props." + action.key;
+  element = _.set(element, key, action.value);
+  return element;
+}
+
+function elementActionAdd(element, action) {
+  element.children.push(action.component);
+  return element;
+}
